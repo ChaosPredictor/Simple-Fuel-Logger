78dcd06764c5fac14edd10b061d38820a93f542b
chaospredictor <kuzi81@gmail.com>
2017-10-03 12:45:35 +0300

    touchesBegan added to MainView

--- Simple Fuel Logger/ViewController.swift
+++ Simple Fuel Logger/ViewController.swift
@@ -49,6 +49,10 @@ class ViewController: UIViewController {
         
 
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
     func resignFirstResponder() {
         print("dsf")
