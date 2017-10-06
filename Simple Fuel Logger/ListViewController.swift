047778c65f00aa132e2eec76e3f22dc5ece442ab
chaospredictor <kuzi81@gmail.com>
2017-10-06 22:16:23 +0300

    table with first cell added to ListViewController

---
+++ Simple Fuel Logger/ListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ListViewController.swift
+//  Simple Fuel Logger
+//
+//  Created by Master on 06/10/2017.
+//  Copyright © 2017 Master. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "RefuelTableViewCell")
+        cell.textLabel?.text = String("ar[indexPath.row]")
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("list view controller loaded")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Simple Fuel Logger/RefuelTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RefuelTableViewCell.swift
+//  Simple Fuel Logger
+//
+//  Created by Master on 06/10/2017.
+//  Copyright © 2017 Master. All rights reserved.
+//
+
+import UIKit
+
+class RefuelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        print("Cell awake from nib")
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
