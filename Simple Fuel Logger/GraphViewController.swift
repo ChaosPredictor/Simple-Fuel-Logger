181d5da0d1ab1c67da603b7fd64140c9fd604c89
chaospredictor <kuzi81@gmail.com>
2017-10-21 22:07:34 +0300

    basic charts example working

--- Simple Fuel Logger/GraphViewController.swift
+++ Simple Fuel Logger/GraphViewController.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import Charts
 
 class GraphViewController: UIViewController {
 
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //let graphView = ScrollableGraphView(frame: frame, dataSource: self)
 
         // Do any additional setup after loading the view.
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        
+        setChart(dataPoints: months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +31,43 @@ class GraphViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        //let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet) {
+        pieChartView.data = pieChartData
+        //}
+        
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        
+        
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Units Sold")
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        //let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+        
+    }
 
     /*
     // MARK: - Navigation
