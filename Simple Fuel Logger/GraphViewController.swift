6797dd20481f85e48e7e263f4a207c571683e8ba
chaospredictor <kuzi81@gmail.com>
2017-10-22 13:06:49 +0300

    pie and line charts show data

--- Simple Fuel Logger/GraphViewController.swift
+++ Simple Fuel Logger/GraphViewController.swift
@@ -21,11 +21,10 @@ class GraphViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
-        //let months2 = ["0.0":"Jan", "1.0":"Feb", "2.0":"Mar", "3.0":"Apr", "4.0":"May", "5.0":"Jun"]
 
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
         
-        setChart(dataPoints: months, values: unitsSold)
+        setChart(labels: months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,36 +33,26 @@ class GraphViewController: UIViewController {
     }
     
     
-    func setChart(dataPoints: [String], values: [Double]) {
-        /*
-        var dataEntries: [ChartDataEntry] = []
+    func setChart(labels: [String], values: [Double]) {
         
-        for i in 0..<dataPoints.count {
-            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
-            //let dataEntry = ChartDataEntry(x: Double(i), y: values[i], data: dataPoints[i] as AnyObject)
-            dataEntries.append(dataEntry)
-        }*/
+        print(values)
+        //let i = [1, 2, 3, 4, 5, 6]
         
-        //let values = Array(1..<10).map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) * 100.0 }
-        let labels = dataPoints
-        let dataEntries = values.enumerated().map { x, y in return PieChartDataEntry(value: y, label: labels[x]) }
+        let dataEntries = values.enumerated().map { index, value in return PieChartDataEntry(value: value, label: labels[index]) }
+
+        
+        
+        print(dataEntries)
         
         let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Units Sold")
-        
-        let data = PieChartData()
-        let ds1 = PieChartDataSet(values: dataEntries, label: "       Hello")
-        data.addDataSet(ds1)
-        //ds1.colors = ChartColorTemplates.liberty()
+        pieChartDataSet.colors = [ChartColorTemplates.colorFromString("#52FFA300"), ChartColorTemplates.colorFromString("#52EDB521") , ChartColorTemplates.colorFromString("#52DBC742"), ChartColorTemplates.colorFromString("#52C9D963") , ChartColorTemplates.colorFromString("#52B7EA84"), ChartColorTemplates.colorFromString("#52A5FFA6") ]
+        pieChartDataSet.valueTextColor = ChartColorTemplates.colorFromString("#FF000000")
 
-        ds1.colors = [ChartColorTemplates.colorFromString("#52FFA300"), ChartColorTemplates.colorFromString("#52EDB521") , ChartColorTemplates.colorFromString("#52DBC742"), ChartColorTemplates.colorFromString("#52C9D963") , ChartColorTemplates.colorFromString("#52B7EA84"), ChartColorTemplates.colorFromString("#52A5FFA6") ]
-        ds1.valueTextColor = ChartColorTemplates.colorFromString("#FF000000")
-        
-        //let pieChartData = PieChartData(dataSet: pieChartDataSet)
-        //let pieChartdata = PieChartDataEntry(
-        //pieChartData.setValuesForKeys(dataPoints)
-        //let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet) {
-        pieChartView.data = data
-        //}
+        let pieChartData = PieChartData()
+        pieChartData.addDataSet(pieChartDataSet)
+
+        pieChartView.data = pieChartData
+
         
         /*
         var colors: [UIColor] = []
@@ -74,11 +63,18 @@ class GraphViewController: UIViewController {
             
             let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
             colors.append(color)
-        }
-        
+        }*/
+
+        var dataEntries2 : [ChartDataEntry] = []
+
+         for index in 0..<values.count {
+         let c = ChartDataEntry(x: Double(index), y: values[index])
+            dataEntries2.append(c)
+         }
+         /*
         pieChartDataSet.colors = colors */
  
-        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Units Sold")
+        let lineChartDataSet = LineChartDataSet(values: dataEntries2, label: "Units Sold")
         let lineChartData = LineChartData(dataSet: lineChartDataSet)
         //let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
         lineChartView.data = lineChartData
